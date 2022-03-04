6133919360e02e4154c9abbe4efdae3a691ba47f
Fabio Vettori <fabio.vettori@ergonresearch.it>
2022-03-04 15:20:02 +0100

    Maven-Spring initialized project

---
+++ src/main/java/com/corattori/measurementunits/MeasurementunitsApplication.java
@@ -0,0 +1,13 @@
+package com.corattori.measurementunits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasurementunitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeasurementunitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corattori/measurementunits/MeasurementunitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corattori.measurementunits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeasurementunitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
